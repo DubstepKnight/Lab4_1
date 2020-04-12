4ddf2aaeed9aaa98669e8f18d44243f4ac06dfe5
DubstepKnight <nursmaster@gmail.com>
2020-04-12 16:20:01 +0300

    the task is done

--- app/src/main/java/image/color/git/lab4_4/MainActivity.java
+++ app/src/main/java/image/color/git/lab4_4/MainActivity.java
@@ -3,8 +3,21 @@ package image.color.git.lab4_4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +26,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void sendRequest(View view) {
+        textView = findViewById(R.id.textView);
+        EditText enterUrl = findViewById(R.id.editText);
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String stringUrl = enterUrl.getText().toString();
+        Log.d("sendRequest", "" + stringUrl);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, stringUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the response
+                        textView.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+        queue.add(stringRequest);
+    }
 
 
 }
